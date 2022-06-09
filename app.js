ccce287cea69c8291c6fd908ada8481b9c8f293f
Cherrelle-Ward <Cherrelle_Ward@hotmail.co.uk>
2022-06-09 16:56:02 +0100

    error fixed

--- app.js
+++ app.js
@@ -16,16 +16,17 @@ const navSlide = () => {
   const nav = document.querySelector(".nav-links");
   const navSocials = document.querySelectorAll(".nav-links li");
 
-  burger.addEventListener("click", () => {
-    // toggle nav slider
+  burger.addEventListener("click", (e) => {
+    console.log("yey");
 
+    // toggle nav slider
     nav.classList.toggle("nav-active");
     // burger animation
     burger.classList.toggle("toggle");
     // animate links
     navSocials.forEach((link, index) => {
       if (link.style.animation) {
-        link.style.animation = "";
+        link.style.animation = " ";
       } else {
         link.style.animation = `navLinkFade 0.5s ease forwards ${
           index / 7 + 1
